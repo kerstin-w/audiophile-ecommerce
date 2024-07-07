2b604292a4877270a933929dbf981e1db9316d65
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-07-07 21:56:25 +0200

    Add all Header component together

--- app/_components/Headers.js
+++ app/_components/Headers.js
@@ -1,15 +1,29 @@
+'use client';
+import { useState } from 'react';
 import Logo from './Logo';
 import NavCart from './NavCart';
-import Navigation from './Navigation';
+import NavigationDesktop from './NavigationDesktop';
+import NavigationMobile from './NavigationMobile';
+import Hamburger from './Hamburger';
 
 function Headers() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className="bg-black text-white">
-      <div className="md:mx-36 md:my-12 flex justify-between border-b border-primary-100 pb-8">
+      <div className="md:mx-36 md:my-12 px-6 py-6 flex justify-between border-b border-primary-100 pb-8">
+        <div className="md:hidden flex items-end mr-5">
+          <Hamburger toggleMenu={toggleMenu} isOpen={isOpen} />
+        </div>
         <Logo />
-        <Navigation />
+        <NavigationDesktop />
         <NavCart />
       </div>
+      <NavigationMobile isOpen={isOpen} />
     </header>
   );
 }
