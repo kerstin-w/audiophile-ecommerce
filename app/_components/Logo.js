87af3cae85bd1c1fae7651d6e292ad14f86d22e0
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-07-07 18:24:28 +0200

    feat: Add Logo component with link to homepage and optimized image
    - Created a functional component named Logo using React.
    - Added a Link component that redirects to the homepage (/).
    - Included an Image component with the following properties:
      - src set to logo (assumed to be imported or defined elsewhere).
      - height set to 25 pixels and width set to 143 pixels for proper sizing.
      - quality set to 90 to ensure high-quality image rendering.
      - alt text set to audiphile Logo for accessibility and SEO purposes.

---
+++ app/_components/Logo.js
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import logo from '@/public/shared/desktop/logo.svg';
+
+function Logo() {
+  return (
+    <Link href="/">
+      <Image
+        src={logo}
+        height="25"
+        width="143"
+        quality={90}
+        alt="audiphile Logo"
+      />
+    </Link>
+  );
+}
+
+export default Logo;
