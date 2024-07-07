5bb7b027f983557094ff1e8445accf3e18e54d14
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-07-07 20:48:03 +0200

    style: Add border to header

--- app/_components/Headers.js
+++ app/_components/Headers.js
@@ -5,7 +5,7 @@ import Navigation from './Navigation';
 function Headers() {
   return (
     <header className="bg-black text-white">
-      <div className="md:mx-36 md:my-12 flex justify-between border-b border-primary-100 pb-6">
+      <div className="md:mx-36 md:my-12 flex justify-between border-b border-primary-100 pb-8">
         <Logo />
         <Navigation />
         <NavCart />
