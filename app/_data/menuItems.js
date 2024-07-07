b65c39f884989251188279162eeef4a5e7fb21cc
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-07-07 18:29:09 +0200

    feat: Create file for menu items

---
+++ app/_data/menuItems.js
@@ -0,0 +1,8 @@
+const menuItems = [
+  { name: 'Home', path: '/' },
+  { name: 'Headphones', path: '/headphones' },
+  { name: 'Earphones', path: '/earphones' },
+  { name: 'Speakers', path: '/speakers' },
+];
+
+export default menuItems;
