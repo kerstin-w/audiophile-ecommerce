c001bcd2ed5635696defd41b91755f35f59500a2
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-07-07 21:51:49 +0200

    refactor: Move navigation items into own component

---
+++ app/_components/NavigationItems.js
@@ -0,0 +1,27 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import menuItems from '../_data/menuItems';
+
+function NavigationItems() {
+  const router = usePathname();
+  return (
+    <>
+      {menuItems.map((item, index) => (
+        <li
+          key={item.path}
+          className={`uppercase hover:text-primary-300 font-bold tracking-wider py-4 md:py-0${
+            router === item.path ? 'text-primary-300 ' : ''
+          } ${index !== menuItems.length - 1 ? 'pr-6' : ''}`}
+        >
+          <Link href={item.path}>
+            <p>{item.name}</p>
+          </Link>
+        </li>
+      ))}
+    </>
+  );
+}
+
+export default NavigationItems;
