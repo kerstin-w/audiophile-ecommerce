68e3381daf6b1e401cda4d8cc36dcb1d8308e262
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-07-08 11:16:13 +0200

    feat: Upload category images and create file for category data

---
+++ app/_data/categories.js
@@ -0,0 +1,19 @@
+const categories = [
+  {
+    name: 'Headphones',
+    imageSrc: '/product-xx59-headphones/desktop/img-category-preview.png',
+    link: '/headphones',
+  },
+  {
+    name: 'Speakers',
+    imageSrc: '/product-zx9-speaker/desktop/img-category-preview.png',
+    link: '/speakers',
+  },
+  {
+    name: 'Earphones',
+    imageSrc: '/product-yx1-earphones/desktop/img-category-preview.png',
+    link: '/earphones',
+  },
+];
+
+export default categories;
