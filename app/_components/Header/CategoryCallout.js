dbffab55b60aec987c1b3584c19dd5795c78f1bf
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-07-08 12:06:18 +0200

    feat: Add CategoryCallout component
    - Created CategoryCallout component to display categories in a responsive grid layout.
    - Utilized Tailwind CSS classes for styling responsiveness and layout.
    - Implemented dynamic rendering of categories using categories.map() method.
    - Each category card includes an image, name, and a 'Shop' link with a corresponding SVG arrow.
    - Adjusted image dimensions for optimal display within the card.
    - Added hover effects to enhance user interaction and visibility.
    
    This component enhances the user experience by presenting categorized content in an organized and visually appealing manner.

---
+++ app/_components/Header/CategoryCallout.js
@@ -0,0 +1,46 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import categories from '@/app/_data/categories';
+
+function CategoryCallout() {
+  return (
+    <div className="flex flex-col md:flex-row xl:mx-36 md:mx-24 mx-12 md:space-x-4 lg:space-x-8 my-[60px] md:my-[120px]">
+      {categories.map((category) => (
+        <div className="flex flex-col items-center bg-primary-100 rounded-lg relative mt-14 w-full shadow-lg">
+          <div className="w-[150px] lg:w-[180px] overflow-hidden absolute transform -translate-y-12 lg:-translate-y-16">
+            <Image
+              src={category.imageSrc}
+              alt={category.name}
+              width={200} // Adjust width as needed
+              height={200} // Adjust height as needed
+              className="object-contain"
+            />
+          </div>
+          <h3 className="text-base lg:text-[18px] font-bold tracking-wide uppercase pt-20 lg:pt-28">
+            {category.name}
+          </h3>
+          <Link
+            href={category.link}
+            className="flex flex-row justify-center items-center py-3"
+          >
+            <p className="text-xs font-semibold uppercase leading-10 tracking-tight opacity-50 mr-3 transition-colors duration-300 hover:text-primary-300 hover:opacity-100">
+              Shop
+            </p>
+            <svg width="8" height="12" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M1.322 1l5 5-5 5"
+                stroke="#D87D4A"
+                stroke-width="2"
+                fill="none"
+                fillRule="evenodd"
+              />
+            </svg>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default CategoryCallout;
