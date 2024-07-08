67293d140712518d56fcbe5587d16d8b74f39fcf
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-07-08 10:56:29 +0200

    feat: Add HeroBanner component with CTA - Integrated Cta component into new HeroBanner component for enhanced navigation - Styled HeroBanner using Tailwind CSS to create a visually appealing layout - Included text elements for product promotion with appropriate styling for emphasis - Ensured responsive design with flexible alignment and spacing adjustments for different screen sizes

---
+++ app/_components/HeroBanner.js
@@ -0,0 +1,23 @@
+import Cta from './Cta';
+
+function HeroBanner() {
+  return (
+    <div className="heroBanner">
+      <div className="xl:mx-36 md:mx-24 md:my-36 md:w-1/3 h-full md:block flex flex-col items-center justify-center">
+        <p className="uppercase tracking-widest text-white font-normal opacity-50 pb-5 text-center md:text-left">
+          New Product
+        </p>
+        <h1 className="text-white font-bold text-6xl leading-tight uppercase text-center md:text-left pb-5">
+          XX99 Mark II HeadphoneS
+        </h1>
+        <p className="font-medium text-[15px] leading-[25px] text-white text-center md:text-left mix-blend-normal opacity-75 w-4/5 pb-5">
+          Experience natural, lifelike audio and exceptional build quality made
+          for the passionate music enthusiast.
+        </p>
+        <Cta path="/">See Product</Cta>
+      </div>
+    </div>
+  );
+}
+
+export default HeroBanner;
--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
-import Image from 'next/image';
+import HeroBanner from './_components/HeroBanner';
 
 export default function Page() {
-  return <h1>Hello World</h1>;
+  return <HeroBanner />;
 }
