0a6820397005a98c2d204988512a73e146e08317
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-07-09 13:45:33 +0200

    feat: Adjust path for images to supabase storeage and add path in next configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'egyohacokvmvqajxhfdx.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
+  // output: 'export',
+};
 
 export default nextConfig;
