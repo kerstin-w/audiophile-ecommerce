c6b6df47e4ce4dacafa23786c1da058bd8809640
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-07-09 19:53:04 +0200

    style: remove transform

--- app/_components/Cta.js
+++ app/_components/Cta.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 function Cta({ children, path }) {
   return (
     <Link href={path}>
-      <p className="inset-0 bg-primary-300 text-white transform -translate-y-1/2 uppercase font-bold text-[13px] leading-[18px] tracking-[1px] transition-colors duration-300 hover:bg-accent-300 w-fit px-7 py-3 mt-5">
+      <p className="inset-0 bg-primary-300 text-white transform uppercase font-bold text-[13px] leading-[18px] tracking-[1px] transition-colors duration-300 hover:bg-accent-300 w-fit px-7 py-3">
         {children}
       </p>
     </Link>
