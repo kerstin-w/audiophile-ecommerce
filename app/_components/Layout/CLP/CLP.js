f12a4ba6064f98e29fa3086e029a8ffeb0aed4f8
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-07-09 15:34:03 +0200

    feat: Make heading for clps h1

--- app/_components/Layout/CLP/CLP.js
+++ app/_components/Layout/CLP/CLP.js
@@ -7,7 +7,7 @@ function CLP({ category }) {
   return (
     <>
       <div className="uppercase bg-primary-200 text-white text-center font-bold text-4xl leading-tight tracking-wider py-12 md:pt-0">
-        {category[0].category}
+        <h1>{category[0].category}</h1>
       </div>
       <div className="xl:mx-36 md:mx-24 mx-12 my-24">
         {category.map((product, index) => (
