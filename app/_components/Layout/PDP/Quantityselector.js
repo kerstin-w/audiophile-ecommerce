510680ff7b3cc93ff127a89dfb174c7e5ef049dc
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-07-11 14:05:26 +0200

    style: Add hover color to Quantityselector

--- app/_components/Layout/PDP/Quantityselector.js
+++ app/_components/Layout/PDP/Quantityselector.js
@@ -1,9 +1,12 @@
 'use client';
+
 import { useState } from 'react';
-import Cta from '../../Cta';
+import BuyButton from '../../BuyButton';
+import { useShoppingCart } from '@/app/_context/ShoppingCartContext';
 
 function QuantitySelector({ product }) {
   const [quantity, setQuantity] = useState(1);
+  const { addItem } = useShoppingCart();
 
   const incrementQuantity = () => {
     setQuantity(quantity + 1);
@@ -15,24 +18,28 @@ function QuantitySelector({ product }) {
     }
   };
 
+  const handleAddToCart = () => {
+    addItem(product, quantity);
+  };
+
   return (
     <div className="flex items-center space-x-4">
       <div className="flex items-center bg-gray-100 px-4 py-2">
         <button
-          className="text-gray-500 focus:outline-none"
+          className="text-gray-500 focus:outline-none hover:text-primary-300"
           onClick={decrementQuantity}
         >
           -
         </button>
         <span className="mx-6">{quantity}</span>
         <button
-          className="text-gray-500 focus:outline-none"
+          className="text-gray-500 focus:outline-none hover:text-primary-300"
           onClick={incrementQuantity}
         >
           +
         </button>
       </div>
-      <Cta path={`/products/${product.slug}`}>See Product</Cta>
+      <BuyButton handleAddToCart={handleAddToCart}>Add to cart</BuyButton>
     </div>
   );
 }
